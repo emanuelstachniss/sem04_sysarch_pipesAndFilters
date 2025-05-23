83c8cb0dd6a444b670313fceec26998fbcf243bf
SanuelMagmeister <manuel.sagmeister20@gmail.com>
2025-05-23 09:20:11 +0200

    all teapots

--- src/main/java/at/fhv/sysarch/lab3/pipeline/PushPipelineFactory.java
+++ src/main/java/at/fhv/sysarch/lab3/pipeline/PushPipelineFactory.java
@@ -3,9 +3,9 @@ package at.fhv.sysarch.lab3.pipeline;
 import at.fhv.sysarch.lab3.animation.AnimationRenderer;
 import at.fhv.sysarch.lab3.obj.Face;
 import at.fhv.sysarch.lab3.obj.Model;
+import at.fhv.sysarch.lab3.rendering.RenderingMode;
 import at.fhv.sysarch.lab3.utils.MatrixUtils;
 import com.hackoeur.jglm.Mat4;
-import com.hackoeur.jglm.Vec;
 import com.hackoeur.jglm.Vec3;
 import com.hackoeur.jglm.Vec4;
 import javafx.animation.AnimationTimer;
@@ -13,14 +13,12 @@ import javafx.scene.canvas.GraphicsContext;
 
 public class PushPipelineFactory {
     public static AnimationTimer createPipeline(PipelineData pd) {
+        GraphicsContext gc = pd.getGraphicsContext();
+
         // TODO: push from the source (model)
-
         // TODO 1. perform model-view transformation from model to VIEW SPACE coordinates
-
         // TODO 2. perform backface culling in VIEW SPACE
-
         // TODO 3. perform depth sorting in VIEW SPACE
-
         // TODO 4. add coloring (space unimportant)
 
         // lighting can be switched on/off
@@ -36,54 +34,91 @@ public class PushPipelineFactory {
 
         // TODO 7. feed into the sink (renderer)
 
-        // returning an animation renderer which handles clearing of the
-        // viewport and computation of the fraction
-        return new AnimationRenderer(pd) {
-            GraphicsContext gc = pd.getGraphicsContext();
-            private float rotationAngle = 0f;
-            // TODO rotation variable goes in here
 
-            /** This method is called for every frame from the JavaFX Animation
-             * system (using an AnimationTimer, see AnimationRenderer).
-             * @param fraction the time which has passed since the last render call in a fraction of a second
-             * @param model    the model to render
-             */
+        return new AnimationRenderer(pd) {
+            private float animationRotation = 0f;
+
+
             @Override
             protected void render(float fraction, Model model) {
                 gc.setStroke(pd.getModelColor());
 
-                // Set up model transformation parameters
+                animationRotation += (float) (fraction * Math.toRadians(10));
+
+                // get the model matrix
                 Vec3 modelPos = pd.getModelPos();
-                Vec3 modelScale = new Vec3(100, 100, 100);
+                Vec3 modelRot = pd.getModelRotAxis();
+                Vec3 modelScale = new Vec3(1,1,1);
 
-                Vec3 yAxis = new Vec3(1, 0, 0);
-
-                // Use a class field to accumulate rotation angle (in radians)
-                // For example, add: private float rotationAngle = 0; at the class level
-                rotationAngle += fraction * Math.PI; // Adjust speed as needed
-
+                // Create Matrix from local position vectors
                 Mat4 scaleMatrix = MatrixUtils.createScalingMatrix(modelScale);
-                Mat4 rotationMatrix = MatrixUtils.createRotationMatrix(yAxis, rotationAngle);
+                Mat4 rotationMatrix = MatrixUtils.createRotationMatrix(modelRot, animationRotation);
                 Mat4 translationMatrix = MatrixUtils.translationMatrix(modelPos);
 
+                // apply the translation, rotation and scale matrix to the model matrix
                 Mat4 modelMatrix = translationMatrix.multiply(rotationMatrix).multiply(scaleMatrix);
 
+                // get the modelViewProjectionMatrix by multiplying them IMPORTANT: Projection * View * Model <- in this step
                 Mat4 viewMatrix = pd.getViewTransform();
                 Mat4 projectionMatrix = pd.getProjTransform();
-
                 Mat4 mvpMatrix = projectionMatrix.multiply(viewMatrix).multiply(modelMatrix);
 
+
+                Mat4 viewportMatrix = pd.getViewportTransform();
+
                 for (Face face : model.getFaces()) {
+                    // multiply matrix with each vertex to get the new translation
                     Vec4 v1 = mvpMatrix.multiply(face.getV1());
                     Vec4 v2 = mvpMatrix.multiply(face.getV2());
                     Vec4 v3 = mvpMatrix.multiply(face.getV3());
 
-                    gc.strokeLine(v1.getX(), v1.getY(), v2.getX(), v2.getY());
-                    gc.strokeLine(v2.getX(), v2.getY(), v3.getX(), v3.getY());
-                    gc.strokeLine(v3.getX(), v3.getY(), v1.getX(), v1.getY());
-                }
-            }
+                    // Perspective divide by W of vertex
+                    v1 = v1.multiply(1f / v1.getW());
+                    v2 = v2.multiply(1f / v2.getW());
+                    v3 = v3.multiply(1f / v3.getW());
 
+                    // Transform viewport to screen coordinates
+                    v1 = viewportMatrix.multiply(v1);
+                    v2 = viewportMatrix.multiply(v2);
+                    v3 = viewportMatrix.multiply(v3);
+
+                    // Draw each line either filled or wireframe render mode
+                    if (pd.getRenderingMode() == RenderingMode.FILLED) {
+
+                        double[] xPoints = {v1.getX(), v2.getX(), v3.getX()};
+                        double[] yPoints = {v1.getY(), v2.getY(), v3.getY()};
+                        gc.setFill(pd.getModelColor());
+                        gc.fillPolygon(xPoints, yPoints, 3);
+
+                    } else if( pd.getRenderingMode() == RenderingMode.WIREFRAME){
+                        gc.setStroke(pd.getModelColor());
+
+                        gc.strokeLine(v1.getX(), v1.getY(), v2.getX(), v2.getY());
+                        gc.strokeLine(v2.getX(), v2.getY(), v3.getX(), v3.getY());
+                        gc.strokeLine(v3.getX(), v3.getY(), v1.getX(), v1.getY());
+
+                    }else if ( pd.getRenderingMode() == RenderingMode.POINT ){
+                        gc.setFill(pd.getModelColor());
+
+                        gc.fillOval(v1.getX() - 0.5, v1.getY() - 0.5, 1, 1);
+                        gc.fillOval(v2.getX() - 0.5, v2.getY() - 0.5, 1, 1);
+                        gc.fillOval(v3.getX() - 0.5, v3.getY() - 0.5, 1, 1);
+                    }
+                }
+
+
+
+                // TODO compute rotation in radians
+
+                // TODO create new model rotation matrix using pd.modelRotAxis
+
+                // TODO compute updated model-view tranformation
+
+                // TODO update model-view filter
+
+                // TODO trigger rendering of the pipeline
+
+            }
         };
     }
 }
